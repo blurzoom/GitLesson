bed2c00a32d216682393c1c21176fa5ffdf9443c
blurzoom <blurzoom@gmail.com>
2024-10-02 15:37:25 +0300

    final change

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let h1=document.getElementsByTagName('h1')[0];
     h1.style.color='red';
     h1.innerText += ` I'm like programmed`;
-    let timer = 0
+    let timer;
     const startText = h1.innerHTML;
     setInterval(() => {
-        timer++;
+        timer = new Date().toLocaleTimeString();
         h1.innerHTML = `${startText}<div>${timer}</div>`
     }, 1000)
 
