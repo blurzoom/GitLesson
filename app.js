4bee097cef4576c7af3a6b23a65b9b3c7af5fb21
blurzoom <blurzoom@gmail.com>
2024-10-02 12:47:35 +0300

    add timer open page

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     let h1=document.getElementsByTagName('h1')[0];
     h1.style.color='red';
+    h1.innerText += ` I'm like programmed`;
+    let timer = 0
+    const startText = h1.innerHTML;
+    setInterval(() => {
+        timer++;
+        h1.innerHTML = `${startText}<div>${timer}</div>`
+    }, 1000)
 
 })
